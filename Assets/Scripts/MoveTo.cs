aea99cd2942271e0f252b926eba8f79276647780
juliuse98 <julius.ewert1@gmail.com>
2024-05-14 21:46:39 +0200

    Add Squirrel Animation

--- Assets/Scripts/MoveTo.cs
+++ Assets/Scripts/MoveTo.cs
@@ -8,13 +8,29 @@ public class MoveTo : MonoBehaviour
     [SerializeField]
     private GameObject targetPosition;
     private Vector3 lastPosition;
+    [SerializeField]
+    private Animator animator;
 
     void Update()
     {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        if (agent == null) return;
+        Debug.Log(agent.velocity.sqrMagnitude);
+        if (agent.velocity.sqrMagnitude > 1.0)
+        {
+
+            animator.SetBool("isWalking", true);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
         if (lastPosition == targetPosition.transform.position) return;
 
         Vector3 newTargetPosition = targetPosition.transform.position;
-        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+
+        // Get current speed of the agent and play animation from that
+        
         agent.destination = newTargetPosition;
 
         lastPosition = newTargetPosition;
