186daea9a3821d744208715dccf263ac014b838a
Noah Thiering <66632359+Noah4ever@users.noreply.github.com>
2024-05-21 12:33:03 +0000

    Add basic house script

--- Assets/Scripts/Updateable/Buildings/House.cs
+++ Assets/Scripts/Updateable/Buildings/House.cs
@@ -6,13 +6,37 @@ public class House : Building
 {
     [SerializeField]
     private int humanCapacity { get; set; }
+    [SerializeField]
+    private List<Human> humansLiving { get; set; }
+    [SerializeField]
+    private List<Lifeform> lifeformsInside { get; }
+    [SerializeField]
+    private bool openForAnimals { get; set; }
 
     void Start()
     {
-        
+        base.Start();
     }
     void Update()
     {
-        
+        base.Update();
+    }
+
+    public override update()
+    {
+        // TODO: do magic here to update reclaimed int 
+        // 1. if more animals then humans
+        // 2. if more animals.presence then humans.presence
+        // 3. ...
+    }
+
+    public LifeformEnter(Lifeform lifeform)
+    {
+        lifeformsInside.Add(lifeform);
+    }
+
+    public LifeformLeave(Lifeform lifeform)
+    {
+        lifeformsInside.Remove(lifeform);
     }
 }
