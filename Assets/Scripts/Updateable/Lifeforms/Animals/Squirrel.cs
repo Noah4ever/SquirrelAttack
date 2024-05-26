51efdf8ed505f99c73dfce908d6e7359d8c1ad71
juliuse98 <julius.ewert1@gmail.com>
2024-05-26 14:23:25 +0200

    Add squirrel actions

--- Assets/Scripts/Updateable/Lifeforms/Animals/Squirrel.cs
+++ Assets/Scripts/Updateable/Lifeforms/Animals/Squirrel.cs
@@ -2,13 +2,18 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using static UnityEngine.UI.Dropdown;
 
 public class Squirrel : Animal
 {
+    public ActionData currentAction;
+    public SquirrelAction currentSquirrelAction;
     // Start is called before the first frame update
     protected override void Start()
     {
         base.Start();
+        currentSquirrelAction = new SquirrelIdel(this);
         actions.Add(ActionType.Move);
         actions.Add(ActionType.Attack);
         actions.Add(ActionType.Follow);
@@ -19,34 +24,48 @@ public class Squirrel : Animal
     protected override void Update()
     {
         base.Update();
+        currentSquirrelAction.update(timeController.GetCurrentTick());
     }
 
 public override void doAction(ActionData actionData)
 {
+    currentSquirrelAction.end();
     switch(actionData.actionType)
     {
         case ActionType.Attack:
             Attack((AttackActionData)actionData);
             break;
         case ActionType.Move:
-            Move((MoveActionData)actionData);
+            currentSquirrelAction = new SquirrelMove(this,((MoveActionData)actionData).targetPosition);
+            
             break;
         case ActionType.Follow:
-            Follow((FollowActionData)actionData);
+            currentSquirrelAction = new SquirrelFollow(this, ((FollowActionData)actionData).targetFollow,timeController.GetCurrentTick());
             break;
         default:
             break;
     }
+        currentSquirrelAction.execute();
 }
-
+    public void setDestination(Vector3 destination) 
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = destination;
+    }
+    public void resetDestination()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.ResetPath();
+    }
     private void Move(MoveActionData actionData)
     {
-        Debug.Log("Squirrel should move to: " + actionData.targetPosition);
     }
 
     private void Attack(AttackActionData actionData)
     {
         Debug.Log("Squirrel should attack: " + actionData.target.transform.position);
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = actionData.target.transform.position;
     }
 
     private void Follow(FollowActionData actionData)
