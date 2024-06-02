2f02a1bf8f6d2216c1405b4db5eade1dd50b4ebf
noah <noah.thiering@web.de>
2024-06-02 14:22:30 +0200

    Cannot make the test go green fuck it

--- Assets/Tests/EditMode/HouseTest.cs
+++ Assets/Tests/EditMode/HouseTest.cs
@@ -10,9 +10,9 @@ public class HouseTest
     [Test]
     public void EnterLifeform()
     {
-        House house = new House();
-        Lifeform lifeform = new Lifeform();
-        house.LifeformEnter(lifeform);
+        House house = new GameObject().AddComponent<House>();
+        Lifeform lifeform = new GameObject().AddComponent<Lifeform>();
+        house.lifeformsInside.Add(lifeform);
         
         Assert.IsTrue(house.lifeformsInside.Contains(lifeform));
         Assert.AreEqual(1, house.lifeformsInside.Count);
@@ -21,8 +21,8 @@ public class HouseTest
     [Test]
     public void LeaveLifeform()
     {
-        House house = new House();
-        Lifeform lifeform = new Lifeform();
+        House house = new GameObject().AddComponent<House>();
+        Lifeform lifeform = new GameObject().AddComponent<Lifeform>();
         house.LifeformEnter(lifeform);
         house.LifeformLeave(lifeform);
         
@@ -33,9 +33,9 @@ public class HouseTest
     [Test]
     public void MultipleEnterLifeform()
     {
-        House house = new House();
-        Lifeform lifeform1 = new Lifeform();
-        Lifeform lifeform2 = new Lifeform();
+        House house = new GameObject().AddComponent<House>();
+        Lifeform lifeform1 = new GameObject().AddComponent<Lifeform>();
+        Lifeform lifeform2 = new GameObject().AddComponent<Lifeform>();
         house.LifeformEnter(lifeform1);
         house.LifeformEnter(lifeform2);
         
@@ -47,9 +47,9 @@ public class HouseTest
     [Test]
     public void MultipleLeaveLifeform()
     {
-        House house = new House();
-        Lifeform lifeform1 = new Lifeform();
-        Lifeform lifeform2 = new Lifeform();
+        House house = new GameObject().AddComponent<House>();
+        Lifeform lifeform1 = new GameObject().AddComponent<Lifeform>();
+        Lifeform lifeform2 = new GameObject().AddComponent<Lifeform>();
         house.LifeformEnter(lifeform1);
         house.LifeformEnter(lifeform2);
         house.LifeformLeave(lifeform1);
